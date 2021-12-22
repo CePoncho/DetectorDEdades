a90e36116118003cbd77063dccfd467b80f69223
Jose Alfonso <Jose Alfonso@DESKTOP-LAVI9KC>
2021-12-22 17:42:11 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                float edad = 0.0f;
+                Console.WriteLine("Escribe tu edad: ");
+                edad = float.Parse(Console.ReadLine());
+                if (edad < 18 && edad > 0)
+                {
+                    Console.WriteLine("Usted es menor de edad");
+                }
+                else if (edad >= 18 && edad <= 59)
+                {
+                    Console.WriteLine("Usted es mayor de edad");
+
+                }
+                else if (edad > 59)
+                    Console.WriteLine("Usted es una persona de la 3ra edad");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Introduzca un numero no un caracter");
+            }
+            Console.ReadLine();  
+        }
+    }
+}      
+
+        
+    
+    
+
---
+++ ConsoleAppSwitch/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppSwitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int edad = 0;
+                Console.WriteLine("Cual es su edad");
+                edad = Int32.Parse(Console.ReadLine());
+                switch (edad)
+                {
+                    case int n when (n > 0 && n < 18):
+                        Console.WriteLine("Usted es menor de edad");
+                        break;
+
+                    case int n when (n <= 59 && n >= 18):
+                        Console.WriteLine("Usted es mayor de edad");
+                        break;
+
+                    case int n when (n > 59):
+                        Console.WriteLine("Usted es una persona de la tercera edad");
+                        break;
+                }
+            }
+                catch (Exception)
+                 {
+                        Console.WriteLine("Introduzca un numero no un caracter");
+                 }
+                         Console.ReadLine();
+
+        }
+    }
+}
